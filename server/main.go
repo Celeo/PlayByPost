2f6b8a2771bae6e7519e50a1f6a945be94938b43
Celeo <celeodor@gmail.com>
2018-01-14 17:18:21 -0800

    PORT env var for Heroku

--- server/main.go
+++ server/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
-	_ "github.com/mattn/go-sqlite3" // sqlite3 driver
+	_ "github.com/mattn/go-sqlite3" // sqolite3 driver
 	"github.com/spf13/viper"
 )
 
 func main() {
 	loadConfig()
-	createTables()
+	// createTables()
 	r := gin.Default()
 	r.GET("/", viewIndex)
-	r.Run(":5000")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+	r.Run(":" + port)
 }
 
 func loadConfig() {
