3db5b2ba1092913679938244216f006cadbff65d
Celeo <celeodor@gmail.com>
2018-02-17 12:39:58 -0800

    Fixed weird overlap on some screen resolutions
    Password and name change cards were overlapping on 'small' viewports

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -3,16 +3,16 @@
     h1.display-2.mb-3 Profile
     v-container(fluid grid-list-md)
       v-layout(row wrap)
-        v-flex(d-flex lg6 md12)
+        v-flex(d-flex lg6 md12 sm12)
           v-card.elevation-5(light)
             v-card-text
               change-password
-        v-flex(d-flex lg6 md12)
+        v-flex(d-flex lg6 md12 sm12)
           v-card.elevation-5(light)
             v-card-text
               change-name
       v-layout(row wrap)
-        v-flex(d-flex lg6 md12)
+        v-flex(d-flex lg6 md12 sm12)
           v-card.elevation-5(light)
             v-card-text
               change-email
