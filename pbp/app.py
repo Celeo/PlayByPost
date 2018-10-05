cc06a296a2ba6cad969ad355f11d3f7dc78c3e6f
Celeo <celeodor@gmail.com>
2018-10-05 16:19:09 -0700

    View stubs and base template

--- pbp/app.py
+++ pbp/app.py
@@ -1,16 +1,54 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from .shared import db
-from .models import *
 
 
 app = Flask(__name__)
 app.config.from_json('config.json')
 db.app = app
 db.init_app(app)
-db.create_all()
 
 
 @app.route('/')
 def index():
-    return 'Index'
+    return render_template('index.html')
+
+
+@app.route('/campaigns')
+def campaigns():
+    return 'VIEW: campaigns'
+
+
+@app.route('/search')
+def search():
+    return 'VIEW: search'
+
+
+@app.route('/glossary')
+def glossary():
+    return 'VIEW: glossary'
+
+
+@app.route('/help')
+def help():
+    return 'VIEW: help'
+
+
+@app.route('/profile/login')
+def profile_login():
+    return 'VIEW: profile/login'
+
+
+@app.route('/profile/join')
+def profile_join():
+    return 'VIEW: profile/join'
+
+
+@app.route('/profile/settings')
+def profile_settings():
+    return 'VIEW: profile/settings'
+
+
+@app.route('/profile/logout')
+def profile_logout():
+    return 'VIEW: profile/logout'
---
+++ pbp/templates/posts.tpl
@@ -0,0 +1,26 @@
+{{define "content"}}
+
+<!-- TODO pagination -->
+
+{{range .posts}}
+  <div class="card rounded shadow mb-2">
+    <div class="card-body">
+      <h5 class="card-title clearfix">
+        <span class="float-left">{{.Name}} ({{.Tag}})</span>
+        <span class="float-right">{{.Date}}</span>
+      </h5>
+      {{.Content}}
+      <div id="rolls">
+        {{range .Rolls}}
+          <div class="roll">
+            {{.String}} => {{.Value}}{{if .IsD20Crit}} (crit){{end}}
+          </div>
+        {{end}}
+      </div>
+    </div>
+  </div>
+{{end}}
+
+<!-- TODO new post widget -->
+
+{{end}}
