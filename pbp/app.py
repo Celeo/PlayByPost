ee297b302a78728ed43016efad853fa2847aa737
Celeo <celeodor@gmail.com>
2018-10-06 12:35:53 -0700

    Moved views into a blueprint

--- pbp/app.py
+++ pbp/app.py
@@ -1,25 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_login import LoginManager
 
 from .shared import db
 from .models import User
+from .blueprint import blueprint as base_blueprint
 
 
-# ==============================
-# Base
-# ==============================
 app = Flask(__name__)
 app.config.from_json('config.json')
 
-# ==============================
-# DB
-# ==============================
 db.app = app
 db.init_app(app)
 
-# ==============================
-# User management
-# ==============================
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -29,49 +21,4 @@ def load_user(user_id):
     return User.get(user_id)
 
 
-# ==============================
-# View routing
-# ==============================
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/campaigns')
-def campaigns():
-    return 'VIEW: campaigns'
-
-
-@app.route('/search')
-def search():
-    return 'VIEW: search'
-
-
-@app.route('/glossary')
-def glossary():
-    return 'VIEW: glossary'
-
-
-@app.route('/help')
-def help():
-    return 'VIEW: help'
-
-
-@app.route('/profile/login', methods=['GET', 'POST'])
-def profile_login():
-    return 'VIEW: profile/login'
-
-
-@app.route('/profile/join')
-def profile_join():
-    return 'VIEW: profile/join'
-
-
-@app.route('/profile/settings')
-def profile_settings():
-    return 'VIEW: profile/settings'
-
-
-@app.route('/profile/logout')
-def profile_logout():
-    return 'VIEW: profile/logout'
+app.register_blueprint(base_blueprint)
---
+++ pbp/blueprint.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, render_template
+
+
+blueprint = Blueprint('base', __name__, template_folder='templates')
+
+
+@blueprint.route('/')
+def index():
+    return render_template('index.html')
+
+
+@blueprint.route('/campaigns')
+def campaigns():
+    return 'VIEW: campaigns'
+
+
+@blueprint.route('/search')
+def search():
+    return 'VIEW: search'
+
+
+@blueprint.route('/glossary')
+def glossary():
+    return 'VIEW: glossary'
+
+
+@blueprint.route('/help')
+def help():
+    return 'VIEW: help'
+
+
+@blueprint.route('/profile/login', methods=['GET', 'POST'])
+def profile_login():
+    return 'VIEW: profile/login'
+
+
+@blueprint.route('/profile/join')
+def profile_join():
+    return 'VIEW: profile/join'
+
+
+@blueprint.route('/profile/settings')
+def profile_settings():
+    return 'VIEW: profile/settings'
+
+
+@blueprint.route('/profile/logout')
+def profile_logout():
+    return 'VIEW: profile/logout'
